747b76c86a5626dffe2603a5fc53189116756fb5
e17o4 <shirley.bookaholic@gmail.com>
2026-08-26 12:44:29 -0400

    Add Fantasy oracle and spark tables

---
+++ scripts/fantasy-oracle-tables.js
@@ -0,0 +1,43 @@
+export const FANTASY_ORACLE_TABLES = [
+  {
+    key: "fantasy-oracle-actions",
+    name: "SN Fantasy - Oracle Actions",
+    formula: "1d12",
+    results: [
+      [1, "Resist / Escape"], [2, "Assist / Pursue"], [3, "Exalt / Transform"], [4, "Discover / Unite"],
+      [5, "Restore / Corrupt"], [6, "Avenge / Assault"], [7, "Destroy / Disrupt"], [8, "Change / Capture"],
+      [9, "Defend / Adapt"], [10, "Discover / Inspire"], [11, "Build / Control"], [12, "Subvert / Escalate"]
+    ]
+  },
+  {
+    key: "fantasy-oracle-descriptions",
+    name: "SN Fantasy - Oracle Descriptions",
+    formula: "1d12",
+    results: [
+      [1, "Bloodstained / Rugged"], [2, "Cracked / Gleaming"], [3, "Crowded / Shattered"], [4, "Murky / Withered"],
+      [5, "Grim / Ancient"], [6, "Silent / Thriving"], [7, "Rusted / Gilded"], [8, "Broken / Haunted"],
+      [9, "Fierce / Crumbling"], [10, "Lush / Primal"], [11, "Arcane / Peaceful"], [12, "War-torn / Abandoned"]
+    ]
+  },
+  {
+    key: "fantasy-oracle-themes",
+    name: "SN Fantasy - Oracle Themes",
+    formula: "1d12",
+    results: [
+      [1, "Prosperity / Separation"], [2, "Protection / Survival"], [3, "Virtue / Despair"], [4, "Liberation / Honor"],
+      [5, "Victory / Chaos"], [6, "Leadership / Faith"], [7, "Haven / Deception"], [8, "Death / Nature"],
+      [9, "Opportunity / Ambition"], [10, "Alliance / Sacrifice"], [11, "Defeat / Truth"], [12, "Revenge / Legacy"]
+    ]
+  },
+  {
+    key: "fantasy-oracle-nouns",
+    name: "SN Fantasy - Oracle Nouns",
+    formula: "1d12",
+    results: [
+      [1, "Cavern / Idol / Valley"], [2, "Altar / Helm / Relic"], [3, "Bow / Banner / Blade"],
+      [4, "Flask / Scroll / Tower"], [5, "Bones / Amulet / Crown"], [6, "Shield / Vault / Pit"],
+      [7, "Goblet / Lantern / Monster"], [8, "Trap / Portal / City"], [9, "Lair / Shard / Ghost"],
+      [10, "Rune / Village / Statue"], [11, "Crypt / Horn / Person"], [12, "Map / Staff / Army"]
+    ]
+  }
+];
